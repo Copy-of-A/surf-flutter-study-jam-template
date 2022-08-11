df2444f10f8c82c4b16e3827c293a1fd36fc2692
Alla <alla@redcollar.ru>
2022-08-11 16:23:38 +0300

    refactoring to make auth work, now chat messages visible

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:surf_practice_chat_flutter/features/auth/exceptions/auth_exception.dart';
 import 'package:surf_practice_chat_flutter/features/auth/models/token_dto.dart';
 import 'package:surf_practice_chat_flutter/features/auth/repository/auth_repository.dart';
 import 'package:surf_practice_chat_flutter/features/chat/repository/chat_repository.dart';
 import 'package:surf_practice_chat_flutter/features/chat/screens/chat_screen.dart';
 import 'package:surf_study_jam/surf_study_jam.dart';
-
-import '../components/AuthForm.dart';
+import '../models/form_model.dart';
 
 /// Screen for authorization process.
 ///
@@ -26,6 +26,10 @@ class AuthScreen extends StatefulWidget {
 
 class _AuthScreenState extends State<AuthScreen> {
   // TODO(task): Implement Auth screen.
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _loginController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,9 +38,121 @@ class _AuthScreenState extends State<AuthScreen> {
         appBar: AppBar(
           title: const Text("Flutter Chat"),
         ),
-        body: AuthForm(),
+        body: Form(
+          key: _formKey,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            // alignment: Alignment.center,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextFormField(
+                  controller: _loginController,
+                  decoration: InputDecoration(
+                      icon: const Icon(Icons.person),
+                      hintText: 'Enter your login',
+                      labelText: 'Логин',
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                        borderSide: const BorderSide(
+                          color: Colors.blue,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                        borderSide: const BorderSide(
+                          color: Colors.blue,
+                        ),
+                      )),
+                  onSaved: (String? value) {
+                    // model.login = value.toString();
+                  },
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                      icon: const Icon(Icons.lock),
+                      hintText: 'Enter your password',
+                      labelText: 'Password',
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                        borderSide: const BorderSide(
+                          color: Colors.blue,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                        borderSide: const BorderSide(
+                          color: Colors.blue,
+                        ),
+                      )),
+                  onSaved: (String? value) {
+                    // model.password = value.toString();
+                  },
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  // obscureText: true,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Validate will return true if the form is valid, or false if
+                      // the form is invalid.
+                      if (_formKey.currentState!.validate()) {
+                        // Process data.
+                        // _formKey.currentState.save();
+                        _submit();
+                      }
+                    },
+                    child: const Text('Submit'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
+  }
+
+  _submit() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Processing Data')),
+    );
+
+    _auth().then((token) {
+      print("my token: $token");
+      _pushToChat(context, token);
+    }).catchError((e) {
+      print("catched error: ${e.message}");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(e.message)),
+      );
+    });
+  }
+
+  Future _auth() async {
+    FormModel model = FormModel(
+        login: _loginController.text.toString(),
+        password: _passwordController.text.toString());
+    final token = await widget.authRepository
+        .signIn(login: model.login, password: model.password);
+    return token;
   }
 
   void _pushToChat(BuildContext context, TokenDto token) {
